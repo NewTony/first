d1f6b01a9d0cc1c73765efa7fd1fe7ef76be8e78
NewTony <kirsanovae74@gmail.com>
2019-03-25 15:28:14 +0500

    Add bloks: header, body, footer. Add styles for bloks (header, body, footer)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="header">
+      <div class="logo">
+        <img style="width: 50px;" alt="Logo" src="./assets/logo.png">
+      </div>
+      <div class="title">
+        Title
+      </div>
+    </div>
+    <div id="body">
+      <HelloWorld/>
+      <PhoneNumbers/>
+    </div>
+    <div id="footer"></div>
   </div>
 </template>
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
+import PhoneNumbers from './components/PhoneNumbers.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    PhoneNumbers
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
+#header {
+  height: 100px;
+  display: block;
+}
+
+#body {
+  display: block;
+}
+
+#footer {
+  height: 100px;
+  display: block;
+}
+
+.title {
+  display: inline-block;
+}
+
+.content {
+
+}
+
+.logo {
+  display: inline-block;
+}
+
 </style>
