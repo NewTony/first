b0d1093f1822106a105c15c34ea78d566f728738
NewTony <kirsanovae74@gmail.com>
2019-03-25 16:55:23 +0500

    Add new component ComponentA

---
+++ src/components/items/ComponentA.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="phones">
+  <p>{{msg}}</p>
+</div>
+</template>
+
+<script>
+export default {
+name: 'ComponentA',
+data: function() {
+  return {
+
+    }
+  },
+props: {
+  msg: {
+    type: String,
+    default: 'Компонент А'
+  }
+},
+components: {
+}
+}
+</script>
