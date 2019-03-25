acf538240d56db2233f52fa01e8d524ee960a950
NewTony <kirsanovae74@gmail.com>
2019-03-25 16:57:03 +0500

    Add style for title

--- src/App.vue
+++ src/App.vue
@@ -57,6 +57,12 @@ export default {
 
 .title {
   display: inline-block;
+  font-size: 2em;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
 }
 
 .content {
